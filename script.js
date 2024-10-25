7827426f09f18286da81f7dd1dfc3448413456b5
YS-05 <yash23sharma05@gmail.com>
2024-10-25 19:48:45 -0400

    Added mode option logic to createGrid and new event handlers for buttons.

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const container = document.querySelector(".container");
 
 let size = 16;
+mode = "normal";
 
-function createGrid(size) {
+function createGrid(size, mode) {
     container.innerHTML = "";
-    let squareSize = 400/size + "px";
+    let squareSize = 800/size + "px";
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = squareSize;
         square.style.height = squareSize;
+        square.style.opacity = 0;
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "grey";
+            if (mode == "normal") {
+                square.style.opacity = 1;
+
+            }
+            else if (mode == "progressive") {
+                let currentOpacity = Number(square.style.opacity);
+                if (currentOpacity < 1) {
+                    square.style.opacity = currentOpacity + 0.1;
+                }
+            }
+            else {
+                square.style.opacity = 1;
+                let red = Math.random() * 255;
+                let green = Math.random() * 255;
+                let blue = Math.random() * 255;
+                square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            }
         })
         container.appendChild(square);
     };
 }
 
-createGrid(size);
+createGrid(size, "normal");
 
-const btn = document.querySelector("button");
+const btn = document.querySelector(".dimbtn");
 btn.addEventListener("click", () => {
     let inpsize = prompt("Please enter a number below 100 for the squares per side of the grid.");
     inpsize = Number(inpsize);
@@ -27,4 +45,19 @@ btn.addEventListener("click", () => {
         size = inpsize;
         createGrid(size);
     }
+})
+
+const pdbtn = document.querySelector(".pdbtn");
+pdbtn.addEventListener("click", () => {
+    createGrid(size, "progressive");
+})
+
+const rdbtn = document.querySelector(".rdbtn");
+rdbtn.addEventListener("click", () => {
+    createGrid(size, "random");
+})
+
+const norbtn = document.querySelector(".norbtn");
+norbtn.addEventListener("click", () => {
+    createGrid(size, "normal");
 })
